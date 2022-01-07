3dfb38a3b12fcb755451d8e576b443a24a4b52c8
Hikaru Ikuta <woodrush924@gmail.com>
2022-01-07 21:14:13 +0900

    Add number-guessing-game.lisp

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ The following program runs on the original SectorLISP:
   Integers are expressed in unary as the number of atoms in a list, such as `(1 1 1)`.
 
 The following program runs on a [fork of SectorLISP](https://github.com/woodrush/sectorlisp/tree/io) with `READ` and `PRINT`:
+- [number-guessing-game.lisp](number-guessing-game.lisp) : An interactive number guessing game where the player guesses a
+  secret number from 1 to 10, using incremental knowledge of whether the guess was less than or greater than the secret number.
 - [basic-repl.lisp](basic-repl.lisp) : A BASIC-subset interpreter with an REPL interface.
   Supports the commands `LIST`, `RUN`, `DISCARD` for editing and running BASIC programs.
 - [repl.lisp](repl.lisp) : An extended Lisp interpreter with an REPL interface.
---
+++ number-guessing-game.lisp
@@ -0,0 +1,89 @@
+((LAMBDA (
+           MAIN GAMELOOP HELPMESSAGE - <= AND CMP PROGN ;;
+         )
+   ((LAMBDA () (QUOTE (THE GAME HAS FINISHED.)))
+    (MAIN)))
+ (QUOTE
+   (LAMBDA ()
+     (PROGN
+       ;; (QUOTE - PROGN IS IMPLEMENTED BY PASSING EXPRESSIONS AS ARGUMENTS
+       ;;          TO AN EMPTY LAMBDA, WHICH ARE EVALUATED SEQUENTIALLY.
+       ;;        - NOTE THAT PROGN ALWAYS RETURNS NIL, NOT ITS FINAL VALUE.
+       ;;          TO EXTRACT THE EVALUATION RESULTS WITHIN A SEQUENCE,
+       ;;          CONS CAN BE USED INSTEAD OF PROGN.
+       ;;        - SINCE ALL VALUES ARE DISCARDED INSIDE PROGN,
+       ;;          COMMENTS CAN BE WRITTEN IN THIS MANNER.
+       ;;        - NOTE THAT `;;` IS A VALID VARIABLE BOUND TO NIL.)
+       (PRINT (QUOTE (LET'S PLAY A NUMBER GUESSING GAME.
+                      I'M THINKING OF A CERTAIN NUMBER BETWEEN 1 AND 10.
+                      SAY A NUMBER, AND I'LL TELL YOU IF IT'S
+                      LESS THAN, GREATER THAN, OR EQUAL TO MY NUMBER.
+                      CAN YOU GUESS WHAT NUMBER I'M THINKING OF?)))
+       ;; (QUOTE - CALLING PRINT WITHOUT ARGUMENTS PRINTS A NEWLINE)
+       (PRINT)
+       (HELPMESSAGE)
+       (GAMELOOP ())
+       (PRINT)
+       (PRINT (QUOTE (DO YOU WISH TO PLAY AGAIN? Y/N:)))
+       (PRINT)
+       (PRINT (QUOTE >))
+       (COND ((EQ (READ) (QUOTE Y))
+              (PROGN (PRINT (QUOTE (ALRIGHT!)))
+                     ;; (QUOTE - INFINITE LOOPS ARE DONE USING RECURSION)
+                     (MAIN)))
+             ((QUOTE T)
+              (PROGN (PRINT (QUOTE (THANK YOU FOR PLAYING!)))
+                     (PRINT)))))))
+ (QUOTE
+   (LAMBDA (NUMTRIES)
+     (PROGN
+       (PRINT)
+       (PRINT (QUOTE NUMBER>))
+       ((LAMBDA (GUESS ANSWER NUMTRIES)
+          (COND ((ATOM GUESS)
+                 (PROGN
+                   ;; (QUOTE - WHEN THE USER INPUTS AN ATOM, SHOW USAGE TEXT.
+                   ;;          NUMTRIES DO NOT INCREASE IN THIS CASE.)
+                   (HELPMESSAGE)
+                   (GAMELOOP (CDR NUMTRIES))))
+                ((EQ (QUOTE <) (CMP GUESS ANSWER))
+                 (PROGN
+                   (PRINT (QUOTE (YOUR GUESS IS LESS THAN MY NUMBER.)))
+                   (GAMELOOP NUMTRIES)))
+                ((EQ (QUOTE >) (CMP GUESS ANSWER))
+                 (PROGN
+                   (PRINT (QUOTE (YOUR GUESS IS GREATER THAN MY NUMBER.)))
+                   (GAMELOOP NUMTRIES)))
+                ((QUOTE T)
+                 (PROGN
+                   (PRINT (QUOTE (THAT'S RIGHT! MY NUMBER IS:)))
+                   (PRINT ANSWER)
+                   (PRINT)
+                   (PRINT (QUOTE (YOU GUESSED CORRECTLY! CONGRATULATIONS!)))
+                   (PRINT)
+                   (PRINT (QUOTE (NUMBER OF TRIES:)))
+                   (PRINT NUMTRIES)
+                   (PRINT)))))
+        (READ)
+        (QUOTE (* * * * * * *))
+        (CONS (QUOTE *) NUMTRIES)))))
+ (QUOTE (LAMBDA ()
+          (PRINT (QUOTE (PLEASE INPUT YOUR NUMBER IN UNARY.
+                         FOR EXAMPLE, 1 IS (*), 3 IS (* * *), ETC.)))))
+ (QUOTE (LAMBDA (N M)
+          (COND ((EQ N NIL) NIL)
+                ((EQ M NIL) N)
+                ((QUOTE T) (- (CDR N) (CDR M))))))
+ (QUOTE (LAMBDA (N M) (EQ NIL (- N M))))
+ (QUOTE (LAMBDA (X Y) (COND (X Y) ((QUOTE T) NIL))))
+ (QUOTE (LAMBDA (N M)
+          (COND ((AND (<= N M) (<= M N)) (QUOTE ==))
+                ((<= N M) (QUOTE <))
+                ((<= M N) (QUOTE >)))))
+ (QUOTE (LAMBDA () NIL))
+ NIL)
+(* * * * *)
+*
+(* * * * * * * *)
+(* * * * * * *)
+N
