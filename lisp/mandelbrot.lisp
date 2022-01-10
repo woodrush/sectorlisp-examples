9f3ce5fa17691bca1aa9b9be5a0feef0aba920e6
Hikaru Ikuta <woodrush924@gmail.com>
2022-01-10 19:47:57 +0900

    Allow overflow for uaddn and umultn

--- lisp/mandelbrot.lisp
+++ lisp/mandelbrot.lisp
@@ -1,38 +1,41 @@
-((LAMBDA (DEBUG u0 _ addh _ addf _ uaddn _ umultn _ + - *)
+((LAMBDA (DEBUG u0 ud0 _ addhalf _ addfull _ uaddnc _ uaddn _ umultn _ + - *)
    ((LAMBDA () ())
-    (PRINT (addh (QUOTE 0) (QUOTE 0)))
-    (PRINT (addh (QUOTE 0) (QUOTE 1)))
-    (PRINT (addh (QUOTE 1) (QUOTE 0)))
-    (PRINT (addh (QUOTE 1) (QUOTE 1)))
+    (PRINT (addhalf (QUOTE 0) (QUOTE 0)))
+    (PRINT (addhalf (QUOTE 0) (QUOTE 1)))
+    (PRINT (addhalf (QUOTE 1) (QUOTE 0)))
+    (PRINT (addhalf (QUOTE 1) (QUOTE 1)))
     (PRINT)
-    (PRINT (addf (QUOTE 0) (QUOTE 0) (QUOTE 0)))
-    (PRINT (addf (QUOTE 1) (QUOTE 0) (QUOTE 0)))
-    (PRINT (addf (QUOTE 0) (QUOTE 1) (QUOTE 0)))
-    (PRINT (addf (QUOTE 1) (QUOTE 1) (QUOTE 0)))
-    (PRINT (addf (QUOTE 0) (QUOTE 0) (QUOTE 1)))
-    (PRINT (addf (QUOTE 1) (QUOTE 0) (QUOTE 1)))
-    (PRINT (addf (QUOTE 0) (QUOTE 1) (QUOTE 1)))
-    (PRINT (addf (QUOTE 1) (QUOTE 1) (QUOTE 1)))
+    (PRINT (addfull (QUOTE 0) (QUOTE 0) (QUOTE 0)))
+    (PRINT (addfull (QUOTE 1) (QUOTE 0) (QUOTE 0)))
+    (PRINT (addfull (QUOTE 0) (QUOTE 1) (QUOTE 0)))
+    (PRINT (addfull (QUOTE 1) (QUOTE 1) (QUOTE 0)))
+    (PRINT (addfull (QUOTE 0) (QUOTE 0) (QUOTE 1)))
+    (PRINT (addfull (QUOTE 1) (QUOTE 0) (QUOTE 1)))
+    (PRINT (addfull (QUOTE 0) (QUOTE 1) (QUOTE 1)))
+    (PRINT (addfull (QUOTE 1) (QUOTE 1) (QUOTE 1)))
     (PRINT)
-    (PRINT (uaddn (QUOTE (1 0 0 1)) (QUOTE (1 0 1 0)) (QUOTE 0)))(PRINT)
+    (PRINT (uaddn (QUOTE (1 0 0 1)) (QUOTE (1 0 1 0))))(PRINT)
     (PRINT)
-    (PRINT (umultn (QUOTE (1 0 0 0 0 0 0)) (QUOTE (1 0 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 0 0 0 0 0 0)) (QUOTE (0 1 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 0 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (0 1 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 1 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 1 1 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 1 0 1 0 0 0)) (QUOTE (1 1 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 1 1 1 0 0 0)) (QUOTE (1 1 1 1 0 0 0))))(PRINT)
+    (PRINT (umultn (QUOTE (1 0 0 0 0 0 0 0)) (QUOTE (1 0 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultn (QUOTE (1 0 0 0 0 0 0 0)) (QUOTE (0 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultn (QUOTE (1 0 0 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultn (QUOTE (0 1 0 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultn (QUOTE (1 1 0 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultn (QUOTE (1 1 1 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultn (QUOTE (1 1 0 1 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultn (QUOTE (1 1 1 1 0 0 0 0)) (QUOTE (1 1 1 1 0 0 0 0))))(PRINT)
+    (PRINT (umultn (QUOTE (1 1 1 1 1 0 0 0)) (QUOTE (1 1 1 1 1 0 0 0))))(PRINT)
     )
    
    
  )
- (QUOTE (LAMBDA (X) ((LAMBDA (_ _ _ Y) Y) (PRINT (QUOTE [)) (PRINT X) (PRINT (QUOTE ])) X)))
- (QUOTE (0 0 0 0 0 0 0))
+ (QUOTE (LAMBDA (X) ((LAMBDA (_ _ _ _ Y) Y) (PRINT (QUOTE [)) (PRINT X) (PRINT (QUOTE ])) (PRINT) X)))
+ (QUOTE (0 0 0 0 0 0 0 0))
+ (QUOTE (0 0 0 0 0 0 0 0
+         0 0 0 0 0 0 0 0))
  (QUOTE
-   ;; addh : Half adder
-   ;;        Output is in binary in reverse order (msb at the end)
+   ;; addhalf : Half adder
+   ;;           Output is in reverse ordered binary (the msb is at the end)
  )
  (QUOTE (LAMBDA (X Y)
    (COND
@@ -43,7 +46,7 @@
      ((QUOTE T)
       (CONS Y (QUOTE 0))))))
  (QUOTE
-   ;; addf : Full adder
+   ;; addfull : Full adder
  )
  (QUOTE (LAMBDA (X Y C)
    ((LAMBDA (XY)
@@ -53,29 +56,34 @@
          ((EQ (QUOTE 1) C) (CONS (QUOTE 0) (QUOTE 1)))
          ((QUOTE T) (CONS (QUOTE 1) (QUOTE 0)))))
       ((QUOTE T) (CONS C (CDR XY)))))
-    (addh X Y))))
+    (addhalf X Y))))
  (QUOTE
-   ;; uaddn : Unsigned N-bit add
+   ;; uaddnc : Unsigned N-bit add with carry
  )
  (QUOTE (LAMBDA (X Y C)
    (COND
-     ((EQ NIL X) NIL)
+     ((EQ NIL X) Y)
+     ((EQ NIL Y) X)
      ((QUOTE T)
       ((LAMBDA (XYC)
-         (CONS (CAR XYC) (uaddn (CDR X) (CDR Y) (CDR XYC))))
-       (addf (CAR X) (CAR Y) C))))))
+         (CONS (CAR XYC) (uaddnc (CDR X) (CDR Y) (CDR XYC))))
+       (addfull (CAR X) (CAR Y) C))))))
+ (QUOTE
+   ;; uaddn : Unsigned N-bit add
+ )
+ (QUOTE (LAMBDA (X Y)
+   (uaddnc X Y (QUOTE 0))))
  (QUOTE
    ;; umultn : Unsigned N-bit mult
  )
  (QUOTE (LAMBDA (X Y)
    (COND
-     ((EQ NIL Y) X)
+     ((EQ NIL Y) u0)
      ((QUOTE T)
       (uaddn (COND
                ((EQ (QUOTE 0) (CAR Y)) u0)
                ((QUOTE T) X))
-             (umultn (CONS (QUOTE 0) X) (CDR Y))
-             (QUOTE 0))))))
+             (umultn (CONS (QUOTE 0) X) (CDR Y)))))))
  (QUOTE
    ;; +
  )
