471235d2ae0f4a9956df6d3014c696d7c457c3d3
Hikaru Ikuta <woodrush924@gmail.com>
2022-01-11 10:28:20 +0900

    Fixed point arithmetic now works

--- lisp/mandelbrot.lisp
+++ lisp/mandelbrot.lisp
@@ -1,5 +1,5 @@
-((LAMBDA (DEBUG u0 umax _ addhalf _ addfull _ uaddnc _ uaddn _ umultn
-          _ take _ drop _ unegate _ + _ - _ *)
+((LAMBDA (DEBUG u0 umax fracbitsize _ addhalf _ addfull _ uaddnofc _ uaddnof _ umultnof
+          _ take _ drop _ unegate _ fixmult _ + _ - _ *)
    ((LAMBDA () ())
     (PRINT (addhalf (QUOTE 0) (QUOTE 0)))
     (PRINT (addhalf (QUOTE 0) (QUOTE 1)))
@@ -15,31 +15,52 @@
     (PRINT (addfull (QUOTE 0) (QUOTE 1) (QUOTE 1)))
     (PRINT (addfull (QUOTE 1) (QUOTE 1) (QUOTE 1)))
     (PRINT)
-    (PRINT (uaddn (QUOTE (1 0 0 1)) (QUOTE (1 0 1 0))))(PRINT)
+    (PRINT (uaddnof (QUOTE (1 0 0 1)) (QUOTE (1 0 1 0))))(PRINT)
     (PRINT)
-    (PRINT (umultn (QUOTE (1 0 0 0 0 0 0 0)) (QUOTE (1 0 0 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 0 0 0 0 0 0 0)) (QUOTE (0 1 0 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 0 0 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (0 1 0 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 1 0 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 1 1 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 1 0 1 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 1 1 1 0 0 0 0)) (QUOTE (1 1 1 1 0 0 0 0))))(PRINT)
-    (PRINT (umultn (QUOTE (1 1 1 1 1 0 0 0)) (QUOTE (1 1 1 1 1 0 0 0))))(PRINT)
+    (PRINT (umultnof (QUOTE (1 0 0 0 0 0 0 0)) (QUOTE (1 0 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultnof (QUOTE (1 0 0 0 0 0 0 0)) (QUOTE (0 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultnof (QUOTE (1 0 0 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultnof (QUOTE (0 1 0 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultnof (QUOTE (1 1 0 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultnof (QUOTE (1 1 1 0 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultnof (QUOTE (1 1 0 1 0 0 0 0)) (QUOTE (1 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT (umultnof (QUOTE (1 1 1 1 0 0 0 0)) (QUOTE (1 1 1 1 0 0 0 0))))(PRINT)
+    (PRINT (umultnof (QUOTE (1 1 1 1 1 0 0 0)) (QUOTE (1 1 1 1 1 0 0 0))))(PRINT)
     (PRINT)
     (PRINT (unegate (QUOTE (1 1 1 1 1 0 0 0))))(PRINT)
     (PRINT (unegate (QUOTE (1 0 0 0 0 0 0 0))))(PRINT)
     (PRINT (unegate (QUOTE (0 1 0 0 0 0 0 0))))(PRINT)
+    (PRINT)
+    (PRINT (+ (QUOTE (0 0 0 0 0 0 0 0   1 0 0 0))
+              (QUOTE (0 0 0 0 0 0 0 0   1 0 0 0))))(PRINT)
+    (PRINT (+ (QUOTE (0 0 0 0 0 0 0 0   1 1 0 0))
+              (QUOTE (0 0 0 0 0 0 0 0   1 0 0 0))))(PRINT)
+    (PRINT (- (QUOTE (0 0 0 0 0 0 0 0   1 1 0 0))
+              (QUOTE (0 0 0 0 0 0 0 0   1 0 0 0))))(PRINT)
+    (PRINT (- (QUOTE (0 0 0 0 0 0 0 0   0 1 0 0))
+              (QUOTE (0 0 0 0 0 0 0 0   1 0 0 0))))(PRINT)
+    (PRINT (- (QUOTE (0 0 0 0 0 0 0 0   1 0 0 0))
+              (QUOTE (0 0 0 0 0 0 0 0   1 0 0 0))))(PRINT)
+    (PRINT (* (QUOTE (0 0 0 0 0 0 0 0   1 0 0 0))
+              (QUOTE (0 0 0 0 0 0 0 0   1 0 0 0))))(PRINT)
+    (PRINT (* (QUOTE (0 0 0 0 0 0 0 1   0 0 0 0))
+              (QUOTE (0 0 0 0 0 0 0 0   1 0 0 0))))(PRINT)
+    (PRINT (* (QUOTE (0 0 0 0 0 0 1 1   0 0 0 0))
+              (QUOTE (0 0 0 0 0 0 0 0   1 1 1 0))))(PRINT)
+    (PRINT (* (QUOTE (0 0 0 0 0 0 1 1   1 1 0 0))
+              (QUOTE (0 0 0 0 0 0 0 0   1 1 1 0))))(PRINT)
     )
    
    
  )
  (QUOTE (LAMBDA (X) ((LAMBDA (_ _ _ _ Y) Y) (PRINT (QUOTE [)) (PRINT X) (PRINT (QUOTE ])) (PRINT) X)))
- (QUOTE (0 0 0 0 0 0 0 0))
+ (QUOTE (0 0 0 0 0 0 0 0   0 0 0 0))
+ (QUOTE (1 1 1 1 1 1 1 1   1 1 1 1))
  (QUOTE (1 1 1 1 1 1 1 1))
  (QUOTE
    ;; addhalf : Half adder
    ;;           Output is in reverse ordered binary (the msb is at the end)
+   ;;           The same applies to the entire system
  )
  (QUOTE (LAMBDA (X Y)
    (COND
@@ -62,7 +83,7 @@
       ((QUOTE T) (CONS C (CDR XY)))))
     (addhalf X Y))))
  (QUOTE
-   ;; uaddnc : Unsigned N-bit add with carry
+   ;; uaddnofc : Unsigned N-bit add with carry
  )
  (QUOTE (LAMBDA (X Y C)
    (COND
@@ -70,24 +91,24 @@
      ((EQ NIL Y) X)
      ((QUOTE T)
       ((LAMBDA (XYC)
-         (CONS (CAR XYC) (uaddnc (CDR X) (CDR Y) (CDR XYC))))
+         (CONS (CAR XYC) (uaddnofc (CDR X) (CDR Y) (CDR XYC))))
        (addfull (CAR X) (CAR Y) C))))))
  (QUOTE
-   ;; uaddn : Unsigned N-bit add
+   ;; uaddnof : Unsigned N-bit add
  )
  (QUOTE (LAMBDA (X Y)
-   (uaddnc X Y (QUOTE 0))))
+   (uaddnofc X Y (QUOTE 0))))
  (QUOTE
-   ;; umultn : Unsigned N-bit mult
+   ;; umultnof : Unsigned N-bit mult
  )
  (QUOTE (LAMBDA (X Y)
    (COND
      ((EQ NIL Y) u0)
      ((QUOTE T)
-      (uaddn (COND
+      (uaddnof (COND
                ((EQ (QUOTE 0) (CAR Y)) u0)
                ((QUOTE T) X))
-             (umultn (CONS (QUOTE 0) X) (CDR Y)))))))
+             (umultnof (CONS (QUOTE 0) X) (CDR Y)))))))
  (QUOTE
    ;; take : Take a list of (len L) atoms from X
  )
@@ -107,24 +128,26 @@
    ;; unegate : Two's complement of unsigned int
  )
  (QUOTE (LAMBDA (N)
-   (take u0 (umultn N umax))))
+   (take u0 (umultnof N umax))))
+ (QUOTE
+   ;; fixmult : Fixed-point multiplication
+ )
+ (QUOTE (LAMBDA (X Y)
+   (take u0 (drop fracbitsize (umultnof X Y)))))
  (QUOTE
    ;; +
  )
  (QUOTE (LAMBDA (X Y)
-   (uaddn X Y (QUOTE 0))
- ))
+   (take u0 (uaddnof X Y (QUOTE 0)))))
  (QUOTE
    ;; -
  )
  (QUOTE (LAMBDA (X Y)
-   ()
- ))
+   (take u0 (uaddnof X (unegate Y) (QUOTE 0)))))
  (QUOTE
    ;; *
  )
  (QUOTE (LAMBDA (X Y)
-   ()
- ))
+   (fixmult X Y)))
 
  )
