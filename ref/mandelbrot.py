fcbdb456c719dd5f9e617fe7df66bb146840fc6e
Hikaru Ikuta <woodrush924@gmail.com>
2022-01-13 17:00:03 +0900

    A working version of the mandelbrot set plotter

--- ref/mandelbrot.py
+++ ref/mandelbrot.py
@@ -1,23 +1,22 @@
-def mandelstep(z_r, z_i, c_r, c_i):
-    z_r_new = z_r*z_r - z_i*z_i + c_r
-    z_i_new = 2 * z_r * z_i + c_i
-    return z_r_new, z_i_new
-
 def isMandel(z_r, z_i, c_r, c_i, N_iter):
     if N_iter == 0:
         return True
     elif z_r * z_r + z_i * z_i > 4:
         return False
     else:
-        return isMandel(*mandelstep(z_r, z_i, c_r, c_i), c_r, c_i, N_iter-1)
+        return isMandel(
+            z_r*z_r - z_i*z_i + c_r,
+            2 * z_r * z_i + c_i,
+            c_r, c_i, N_iter-1)
 
 N_plotsize = 4
 N_iter = 4
-for i in range(1,N_plotsize):
-    for j in range(1,N_plotsize):
-        c_r = -3.0 + 4.0/N_plotsize*i
-        c_i = -1.0 + 2.0/N_plotsize*j
-
+delta_r = 1.0/N_plotsize
+delta_i = 2.0/N_plotsize
+c_i = -1.25
+while c_i <= 1.25:
+    c_r = -2.25
+    while c_r <= 0.5:
         z_r = 0
         z_i = 0
 
@@ -25,4 +24,6 @@ for i in range(1,N_plotsize):
             print("*", end="")
         else:
             print(" ", end="")
+        c_r += delta_r
     print()
+    c_i += delta_i
